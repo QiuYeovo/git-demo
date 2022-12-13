5a99b4658fce3b40a7ff99241e157ed8f26ff780
LinQiu <LinQiuovo@gmail.com>
2022-12-13 19:14:36 +0800

    Lin 2

--- src/main/java/testDemo.java
+++ src/main/java/testDemo.java
@@ -2,5 +2,6 @@ public class testDemo {
     public static void main(String[] args) {
         System.out.println("linqiu6666");
         System.out.println("linqiu1");
+        System.out.println("linqiu2");
     }
 }
